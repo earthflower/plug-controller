d6c3edef68387de8a30be72881571ec2aa4f1e18
aleacevedo <ale.acevedo@live.com.ar>
2021-06-22 13:10:09 +0200

    feat: accept identity and accountId

--- src/PlugWallet/index.ts
+++ src/PlugWallet/index.ts
@@ -1,42 +1,62 @@
-import { Ed25519KeyIdentity } from "@dfinity/identity";
-import { createAccountFromMnemonic } from "../utils/account";
+import { Ed25519KeyIdentity } from '@dfinity/identity';
+import {
+  createAccountFromMnemonic,
+  createAccountFromImported,
+} from '../utils/account';
 
 interface PlugWalletArgs {
-    name?: string;
-    walletNumber: number;
-    mnemonic: string;
+  name?: string;
+  walletNumber: number;
+  mnemonic: string;
+  identity?: string[];
+  accountId?: string;
 }
 
 class PlugWallet {
-    name: string;
-    walletNumber: number;
-    accountId: string;
-    private _identity: Ed25519KeyIdentity;
-    
-    constructor ({ name, walletNumber, mnemonic }: PlugWalletArgs) {
-        this.name = name || 'Main IC Wallet'
-        this.walletNumber = walletNumber;
-        const { identity, accountId } = createAccountFromMnemonic(mnemonic, walletNumber);
-        this._identity = identity;
-        this.accountId = accountId;
-    }
-    get keys() {
-        return this._identity.getKeyPair();
-    }
-    get principal() {
-        return this._identity.getPrincipal();
-    }
+  name: string;
+  walletNumber: number;
+  accountId: string;
+  private _identity: Ed25519KeyIdentity;
 
-    set setName(val: string) {
-        this.name = val;
-    }
+  constructor({
+    name,
+    walletNumber,
+    mnemonic,
+    ...importedArgs
+  }: PlugWalletArgs) {
+    this.name = name || 'Main IC Wallet';
+    this.walletNumber = walletNumber;
 
-    public toJSON = ()  => ({
-        name: this.name,
-        walletNumber: this.walletNumber,
-        identity: this._identity.toJSON(),
-        accountId: this.accountId
-    });
+    const { identity, accountId } =
+      importedArgs.identity && importedArgs.accountId
+        ? createAccountFromImported(
+            mnemonic,
+            importedArgs.identity,
+            importedArgs.accountId
+          )
+        : createAccountFromMnemonic(mnemonic, walletNumber);
+    this._identity = identity;
+    this.accountId = accountId;
+  }
+
+  get keys() {
+    return this._identity.getKeyPair();
+  }
+
+  get principal() {
+    return this._identity.getPrincipal();
+  }
+
+  set setName(val: string) {
+    this.name = val;
+  }
+
+  public toJSON = () => ({
+    name: this.name,
+    walletNumber: this.walletNumber,
+    identity: this._identity.toJSON(),
+    accountId: this.accountId,
+  });
 }
 
 export default PlugWallet;

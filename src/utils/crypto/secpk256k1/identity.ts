8b2b9af1608e4ba1c17774b5635e89e101f7d02b
Tomas Rocchi <tomasrocchi@Tomass-MacBook-Pro.local>
2021-07-28 18:36:12 -0300

    Fix merge errors

--- src/utils/crypto/secpk256k1/identity.ts
+++ src/utils/crypto/secpk256k1/identity.ts
@@ -1,28 +1,12 @@
 /* eslint-disable no-underscore-dangle */
-<<<<<<< Updated upstream
-/* eslint-disable @typescript-eslint/camelcase */
-import Secp256k1 from 'secp256k1';
-
-=======
->>>>>>> Stashed changes
 import {
   blobFromHex,
   blobFromUint8Array,
   blobToHex,
   BinaryBlob,
-  blobFromBuffer,
 } from '@dfinity/candid';
-<<<<<<< Updated upstream
-import {
-  HttpAgentRequest,
-  PublicKey,
-  requestIdOf,
-  SignIdentity,
-} from '@dfinity/agent';
-=======
 import { PublicKey, SignIdentity } from '@dfinity/agent';
 import * as Secp256k1 from 'noble-secp256k1';
->>>>>>> Stashed changes
 import Secp256k1PublicKey from './publicKey';
 
 declare type PublicKeyHex = string;
@@ -146,30 +130,6 @@ class Secp256k1KeyIdentity extends SignIdentity {
     const message = new Uint8Array([...padding, ...challenge]);
     const signature = await Secp256k1.sign(message, this._privateKey);
     return blobFromUint8Array(signature);
-  }
-
-  /**
-   * Transform a request into a signed version of the request. This is done last
-   * after the transforms on the body of a request. The returned object can be
-   * anything, but must be serializable to CBOR.
-   * @param request - internet computer request to transform
-   */
-  public async transformRequest(request: HttpAgentRequest): Promise<unknown> {
-    const { body, ...fields } = request;
-    const requestId = await requestIdOf(body);
-    const domainSeparator = Buffer.from(
-      new TextEncoder().encode('\x0Aic-request')
-    );
-    return {
-      ...fields,
-      body: {
-        content: body,
-        sender_pubkey: this.getPublicKey().toDer(),
-        sender_sig: await this.sign(
-          blobFromBuffer((Buffer as any).concat([domainSeparator, requestId]))
-        ),
-      },
-    };
   }
 }
 
